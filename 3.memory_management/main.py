e567ed1d0d1478fc809e098a8f49d0535928a596
rwuniard <rwuniard@hotmail.com>
2025-08-12 13:50:50 -0400

    adding prompt, chain and interacting with user

--- 3.memory_management/main.py
+++ 3.memory_management/main.py
@@ -1,14 +1,35 @@
-from langchain.prompts import HumanMessagePromptTemplate, ChatMessagePromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 def main():
     print("Hello from 3-memory-management!")
+
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+    # Create a prompt template that has input variables content and human message prompt template.
+    prompt = ChatPromptTemplate(
+        input_variables=["content"],
+        messages = [
+            HumanMessagePromptTemplate.from_template("{content}")
+        ]
+    )
+
+    # Create a chain
+    chain = prompt | llm | StrOutputParser()
+
     while True:
         user_input = input(">> ")
         if user_input == "exit":
             break
-        print(f"You entered: {user_input}")
+        result = chain.invoke({"content": user_input})
+        print(f"AI response: {result}")
 
 
 if __name__ == "__main__":
